88425a3b1734d2b1b4f60a3d45c8a8b8fd22ae15
Shohta Kouchi <shohta.kouchi@gmail.com>
2024-07-15 11:19:13 +0900

    feat: Add initial Post Create script for Dev Container
    This commit introduces a basic structure for the Post Create script in
    the Dev Container, laying the groundwork for future functionalities.

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: Unlicense
+
+# @name Dev Container Post Create Script
+# @brief Script to execute after creating the Dev Container.
+# @description This script is used to execute after creating the Dev Container.
+# @example
+# 	"postCreateCommand": "./.devcontainer/post_create.sh"
+
+# @brief Restore the dependencies.
+# @noargs
+function restore_dependencies() {
+	:
+}
+
+# @brief Change the ownership of the volumes.
+# @noargs
+# @description Docker volumes are created with root ownership. This function changes the ownership of the volumes to the current user.
+function change_volume_ownership() {
+	:
+}
+
+# @brief Main function.
+# @arg $@ The arguments
+function main() {
+
+	# Change the ownership of the volumes.
+	change_volume_ownership
+
+	# Restore the dependencies.
+	restore_dependencies
+}
+
+# Execute the main function if this script is executed directly.
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+
+	main "$@"
+fi
